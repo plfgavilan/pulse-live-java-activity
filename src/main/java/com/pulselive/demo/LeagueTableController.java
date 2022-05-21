5cd035c4eb5ae5ea0034ac0a448d6cdb42c69ab5
plfgavilan <pedrocantoria@gmail.com>
2022-05-21 20:24:44 +0200

    Extract class: controller supervisor injecting the repository and view (DI principle)

---
+++ src/main/java/com/pulselive/demo/LeagueTableController.java
@@ -0,0 +1,21 @@
+package com.pulselive.demo;
+
+import java.util.List;
+
+public class LeagueTableController {
+
+    private final MatchRepository matchRepository;
+    private final LeagueTableView leagueTableView;
+
+    public LeagueTableController(MatchRepository matchRepository, LeagueTableView leagueTableView) {
+        this.matchRepository = matchRepository;
+        this.leagueTableView = leagueTableView;
+    }
+
+    public void getTableEntries(String identifier) {
+        List<Match> matches = matchRepository.readAll(identifier);
+        LeagueTable leagueTable = new LeagueTable(matches);
+        List<LeagueTableEntry> result = leagueTable.getTableEntries();
+        leagueTableView.present(result);
+    }
+}
--- src/main/java/com/pulselive/demo/PulseLiveJavaActivityApplication.java
+++ src/main/java/com/pulselive/demo/PulseLiveJavaActivityApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
 @SpringBootApplication
 @Log4j2
 public class PulseLiveJavaActivityApplication implements CommandLineRunner {
@@ -17,11 +15,11 @@ public class PulseLiveJavaActivityApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        MatchRepository matchRepository = new MatchRepositoryDataHubSportDataImpl();
-        List<Match> matches = matchRepository.readAll("/home/pedro/workspace/pulse-live-java-activity/data/la-liga-0910.csv");
-        LeagueTable leagueTable = new LeagueTable(matches);
-        List<LeagueTableEntry> result = leagueTable.getTableEntries();
-        LeagueTableView leagueTableView = new LeagueTableView();
-        leagueTableView.present(result);
+        LeagueTableController leagueTableController = new LeagueTableController(
+                new MatchRepositoryDataHubSportDataImpl(),
+                new LeagueTableView()
+        );
+        leagueTableController.getTableEntries("/home/pedro/workspace/pulse-live-java-activity/data/la-liga-0910.csv");
+
     }
 }
